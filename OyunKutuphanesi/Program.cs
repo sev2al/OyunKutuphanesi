c242524e60e736d1e977a0b41167d87aa16a92d8
serkan alçay <sakay1514@gmail.com>
2025-05-17 17:48:03 +0300

    ilk commit

--- OyunKutuphanesi/Program.cs
+++ OyunKutuphanesi/Program.cs
@@ -19,6 +19,7 @@ namespace OyunKutuphanesi
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormGirisEkrani());
+
         }
     }
 }
